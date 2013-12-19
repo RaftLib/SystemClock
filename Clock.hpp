b6f3b0061c117a1d9553deb1b1d74a1055a718e4
Jonathan Beard <jonathan.c.beard@gmail.com>
2013-12-19 16:35:57 -0600

    updates

--- Clock.hpp
+++ Clock.hpp
@@ -7,11 +7,17 @@
 #define _CLOCK_HPP_  1
 
 class ClockBase{
+public:
    ClockBase() = default;
    virtual ~ClockBase() = default;
+   
+   virtual  void    init()  = 0;
+   virtual  void    reset() = 0;
 
-
-
+   virtual  clock_t getTime();
+   virtual  clock_t getResolution();
+protected:
+   volatile clock_t *clock;
 };
 
 #endif /* END _CLOCK_HPP_ */
