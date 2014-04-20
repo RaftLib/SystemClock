5113ca5f3a4fa83def6a2262a3f3456bf3405a5b
jonathan-beard <jonathan.c.beard@gmail.com>
2014-04-20 15:36:01 -0500

    updates

--- MacClock.cpp
+++
@@ -1,6 +0,0 @@
-/**
- * MacClock.cpp - 
- * @author: Jonathan Beard
- * @version: Fri Jan 10 14:08:54 2014
- */
-#include "MacClock.hpp"
--- MacClock.hpp
+++
@@ -1,9 +0,0 @@
-/**
- * MacClock.hpp - 
- * @author: Jonathan Beard
- * @version: Fri Jan 10 14:08:54 2014
- */
-#ifndef _MACCLOCK_HPP_
-#define _MACCLOCK_HPP_  1
-
-#endif /* END _MACCLOCK_HPP_ */
