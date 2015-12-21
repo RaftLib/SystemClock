b4946f3e3ffc6449cf146b241f4447a2328e6d79
Jonathan Beard <jonathan.c.beard@gmail.com>
2015-12-21 11:04:50 -0600

    updates

--- testsuite/teststop.cpp
+++ testsuite/teststop.cpp
@@ -6,34 +6,37 @@
 #include <fstream>
 #include <array>
 #include <cassert>
+#include <cmath>
+
 #include "SystemClock.tcc"
 
 int 
 main( int argc, char **argv )
 {
-#if __linux
-   SystemClock< Cycle  > clock( 0 /** core zero **/ );  
-#else
    SystemClock< System > clock( 0 /** core zero **/ );   
-#endif
    const auto resolution( clock.getResolution() );
    const auto microseconds( 1000 );
-   const auto num_iterations( 1e3 );
+   const auto num_iterations( 1e5 );
    double diff_accum( 0.0 );
    for( auto iterations( 0 ); iterations < num_iterations; iterations++ )
    {
-      auto start( clock.stop() );
+      volatile auto start( clock.stop() );
       usleep( (useconds_t) microseconds );
-      const auto diff( clock.start() - start );
+      volatile const auto diff( clock.start() - start );
       diff_accum += diff;
-      std::fprintf( stderr, "%f\n", diff );
    }
    const double average( diff_accum / num_iterations );
-   std::fprintf( stderr, "%f\n", average );
-   /** 
-    * 10x is pretty much completely arbitrary and a bad swap
-    * will result in this test failing
-    */
-   assert( average < 10 * resolution );
+   const double tenperc( .001 /** microseconds **/ * .10 );
+   if( std::isgreater( average, tenperc ) )
+   {
+      std::fprintf( stderr, 
+                    "%.20f is greater than 10 percent (100 microseconds) of measured time.", 
+                    average );
+      std::exit( EXIT_FAILURE );
+   }
+   const double actual_fraction( ( average / .001 ) * 100 );
+   //print out the actual fraction of time we're off
+   std::fprintf( stderr, "%.20f\n", actual_fraction );
+   /** else return success **/
    return( EXIT_SUCCESS );
 }
